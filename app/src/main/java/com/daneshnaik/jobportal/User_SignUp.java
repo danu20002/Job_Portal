9827fb74fc3e0119d89a389f989c9c2e6953e3cf
Danesh Naik <naikdanesh2@gmail.com>
2023-10-24 19:16:15 +0530

    added now

---
+++ app/src/main/java/com/daneshnaik/jobportal/User_SignUp.java
@@ -0,0 +1,14 @@
+package com.daneshnaik.jobportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_sign_up);
+    }
+}
