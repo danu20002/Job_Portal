8d3ae5e4a3a0d2473ebde4aeba2f52a419d9291e
Danesh Naik <naikdanesh2@gmail.com>
2023-10-25 17:41:37 +0530

    added now

--- app/src/main/java/com/daneshnaik/jobportal/MainActivity.java
+++ app/src/main/java/com/daneshnaik/jobportal/MainActivity.java
@@ -2,13 +2,24 @@ package com.daneshnaik.jobportal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView new_user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        new_user=findViewById(R.id.text_new_user);
+//        new_user.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(getApplicationContext(), User_SignUp.class));
+//
+//            }
+//        });
     }
 }
